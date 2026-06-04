5d2fc756ccdcc11c9c10b1e9d142d6434cfad69d
Samuray Jax <recepcakan773@gmail.com>
2026-06-04 17:43:30 +0300

    Sessizlik komutu eklendi

--- src/main/java/com/example/examplemod/ChatEventHandler.java
+++ src/main/java/com/example/examplemod/ChatEventHandler.java
@@ -4,6 +4,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.event.ServerChatEvent;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 
 public class ChatEventHandler {
@@ -13,12 +14,25 @@ public class ChatEventHandler {
         event.setCanceled(true);
 
         EntityPlayerMP sender = event.getPlayer();
-        TextComponentString message = new TextComponentString(
-                "<" + sender.getName() + "> " + event.getMessage()
-        );
+        String chatMessage = event.getMessage();
+        double distanceLimitSq = 2500.0D;
+        TextComponentString message;
+
+        if (chatMessage.startsWith("/S ")) {
+            String whisperMessage = chatMessage.substring(3);
+            distanceLimitSq = 100.0D;
+            message = new TextComponentString(
+                    "[Sessiz] <" + sender.getName() + ">: " + whisperMessage
+            );
+            message.getStyle().setColor(TextFormatting.DARK_GRAY);
+        } else {
+            message = new TextComponentString(
+                    "<" + sender.getName() + "> " + chatMessage
+            );
+        }
 
         for (EntityPlayerMP player : FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayers()) {
-            if (player.dimension == sender.dimension && player.getDistanceSq(sender) <= 2500.0D) {
+            if (player.dimension == sender.dimension && player.getDistanceSq(sender) <= distanceLimitSq) {
                 player.sendMessage(message);
             }
         }
